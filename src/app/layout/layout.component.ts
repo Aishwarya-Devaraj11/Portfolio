e320aa38861dc11c90b12ece50c5614c0e79e6c9
Aishwariya-Devaraj11 <Aishwarya111728@gmail.com>
2025-04-28 14:56:16 +0530

    fixed aos bugs

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import Aos from 'aos';
+// import Aos from 'aos';
 
 @Component({
   selector: 'app-layout',
@@ -12,6 +12,17 @@ import Aos from 'aos';
 export class LayoutComponent implements OnInit{
 
 
+  ngOnInit(): void {
+    // if (typeof window !== 'undefined') {
+    //   import('aos').then((AOS) => {
+    //     AOS.init({
+    //       offset: 0,
+    //       duration: 800,
+    //       once: true
+    //     });
+    //   });
+    // }
+  }
 
 
 
@@ -20,23 +31,13 @@ export class LayoutComponent implements OnInit{
   constructor(private route:Router){}
 
 
-  ngOnInit(): void {
-    if (typeof window !== 'undefined') {
-      import('aos').then((AOS) => {
-        AOS.init({
-          offset: 0,
-          duration: 800,
-          once: true
-        });
-      });
-    }
 
-    this.route.events.subscribe((event: any) => {
-      if (event instanceof NavigationEnd) {
-        Aos.refresh();  // Refresh AOS on every route change
-      }
-    });
-  }
+    // this.route.events.subscribe((event: any) => {
+    //   if (event instanceof NavigationEnd) {
+    //     Aos.refresh();  // Refresh AOS on every route change
+    //   }
+    // });
+
 
 
 
